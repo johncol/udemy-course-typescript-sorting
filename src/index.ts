79bf9f47de28e51ece3f27c30968d24aad03987d
John <john.19col@gmail.com>
2019-11-05 11:36:42 -0500

    Create initial version of Sorter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-const hello: string = 'Happy Halloween!!';
+import { Sorter } from './Sorter';
 
-console.log(hello);
+const sorter: Sorter = new Sorter([6, 1, 0, -5, 55, 10]);
+console.log(sorter.collection);
+sorter.sort();
+console.log(sorter.collection);
