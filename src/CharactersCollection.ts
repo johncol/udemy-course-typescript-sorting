8ffbc3b319d634aa703b94f23f80f41ffe2b89aa
John <john.19col@gmail.com>
2019-11-06 22:54:10 -0500

    Add CharactersCollection class

---
+++ src/CharactersCollection.ts
@@ -0,0 +1,25 @@
+export class CharactersCollection {
+  private letters: string[];
+
+  constructor(string: string) {
+    this.letters = string.split('');
+  }
+
+  get length(): number {
+    return this.letters.length;
+  }
+
+  firstIsGreaterThanSecond = (i: number, j: number): boolean => {
+    return this.letters[i].toLowerCase() > this.letters[j].toLowerCase();
+  };
+
+  swap = (i: number, j: number): void => {
+    const iValue: string = this.letters[i];
+    this.letters[i] = this.letters[j];
+    this.letters[j] = iValue;
+  };
+
+  print = (): void => {
+    console.log(this.letters.join(''));
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const collection: NumbersCollection = new NumbersCollection([6, 1, 0, -5, 55, 10]);
-const sorter: Sorter = new Sorter(collection);
-collection.print();
+const numbers: NumbersCollection = new NumbersCollection([6, 1, 0, -5, 55, 10]);
+numbers.print();
+new Sorter(numbers).sort();
+numbers.print();
 
-sorter.sort();
-collection.print();
+const chars: CharactersCollection = new CharactersCollection('gjfngjkbkJBJbsdhjbiAS');
+chars.print();
+new Sorter(chars).sort();
+chars.print();
